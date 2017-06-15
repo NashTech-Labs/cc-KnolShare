daf5759087761648ad03eae5aed51431925f260f
SangeetaGulia <sangeeta.gulia@knoldus.in>
2017-06-15 23:04:15 +0530

    Closed #6 Added functionality to fetch data from twitter for processing

--- processing/app/com/knoldus/Global.scala
+++ processing/app/com/knoldus/Global.scala
@@ -6,15 +6,12 @@ import play.api.{Application, GlobalSettings}
 object Global extends GlobalSettings{
 
   override def onStart(app: Application): Unit = {
-    println("\n\n\n >>>>>>>>>>>>>>> Processing Global has started running >>>>>>>>>>>>>>> \n\n\n")
     new TwitterFeed().sendTweetsToKafka()
     HashTagInitialiser.hashTagCounter()
 
     val configReader = new TwitterConfigReader()
     val kafkaTopic = configReader.getKStreamTopic()
     val kafkaGroupId = "my-group"
-    println("Kafka Topics: " + kafkaTopic)
-    println("Kafka Group ID: " + kafkaGroupId)
     new TweetConsumer().consumeTweets(kafkaGroupId, kafkaTopic)
   }
 
