7100082f98afee21df1f2f0db00e7dcbb3e338da
Sangeeta Gulia <sangeeta.gulia@knoldus.in>
2017-06-21 17:30:12 +0530

    Refactored code to replace Global module with Eager Binding.
    * Refactored code and added circle.yml file #22
    
    * Removed scalastyle warnings

---
+++ backend/processing/app/com/knoldus/streaming/Launcher.scala
@@ -0,0 +1,9 @@
+package com.knoldus.streaming
+
+import com.google.inject.AbstractModule
+
+class Launcher extends AbstractModule {
+  override def configure(): Unit = {
+    bind(classOf[TweetLauncher]).asEagerSingleton()
+  }
+}
