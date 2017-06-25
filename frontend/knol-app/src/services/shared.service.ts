fec4310a5e55e564f92b59a54338645ad6bf4fe5
AkshayRana92 <akshayrana@knoldus.in>
2017-06-25 18:29:08 +0530

    Added shared service

---
+++ frontend/knol-app/src/services/shared.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core'
+
+@Injectable()
+export class SharedService {
+  isLoggedIn: boolean = false;
+
+  constructor() {
+    if (localStorage.getItem('user')) {
+      this.isLoggedIn = true;
+    }
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    localStorage.clear();
+  }
+}
