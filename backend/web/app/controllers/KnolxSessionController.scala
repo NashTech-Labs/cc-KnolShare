fcaec12cb98b556b3301f07cae6ce43f4717d120
SangeetaGulia <sangeeta.gulia@knoldus.in>
2017-07-03 17:45:09 +0530

    Added web layer functionality to get all KnolxSession

--- backend/web/app/controllers/KnolxSessionController.scala
+++ backend/web/app/controllers/KnolxSessionController.scala
@@ -4,35 +4,39 @@ import java.sql.Date
 
 import com.google.inject.Inject
 import com.knoldus.models.KnolxSession
-import com.knoldus.utils.JsonResponse
+import com.knoldus.utils.{JsonResponse, LoggerHelper}
 import controllers.security.SecuredAction
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.json.{JsString, Json}
 import play.api.mvc.{Action, AnyContent, Controller, Request}
 import service.KnolxSessionService
 
+import scala.util.control.NonFatal
+
 
 class KnolxSessionController @Inject()(knolxSessionService: KnolxSessionService, jsonResponse: JsonResponse) extends
-  Controller with SecuredAction {
+  Controller with SecuredAction with LoggerHelper {
 
-  def createKnolxSession: Action[AnyContent] = {
-    UserAction.async { implicit request =>
-      val (presenter: String, topic: String, sessionId: Int, rating: Int,
-      scheduledDate: Long) = extractJsonFromRequest(request)
-      val topicProvided = topic match {
-        case "" => None
-        case _ => Some(topicProvided)
-      }
-      val (knolxSessionId, knolxRating) = getSessionData(sessionId, rating)
-      knolxSessionService.createKnolxSession(KnolxSession(-1, presenter, topicProvided, knolxSessionId, knolxRating, new Date(scheduledDate))).map {
-        (knolxSession: KnolxSession) => Ok(jsonResponse.successResponse(Json.obj("message" -> JsString("Knolx session successfully registered"))))
+  def createKnolxSession: Action[AnyContent] = UserAction.async { implicit request =>
+    val (presenter: String, topic: String, sessionId: Int, rating: Int, scheduledDate: Long) = extractJsonFromRequest(request)
+    val topicProvided = topic match {
+      case "" => None
+      case _ => Some(topicProvided)
+    }
+    val (knolxSessionId, knolxRating) = getSessionData(sessionId, rating)
+    knolxSessionService.createKnolxSession(KnolxSession(-1, presenter, topicProvided, knolxSessionId, knolxRating,
+      new Date(scheduledDate))).map { knolxSession =>
+      Ok(jsonResponse.successResponse(Json.obj("message" -> JsString("Knolx session successfully registered"), "knolx" -> Json.toJson(knolxSession))))
+    } recover {
+      case NonFatal(ex) => {
+        getLogger(this.getClass).error(s"\n\n ${ex.getMessage}")
+        InternalServerError("Internal Server Error")
       }
     }
   }
 
   private def extractJsonFromRequest(request: Request[AnyContent]): (String, String, Int,
     Int, Long) = {
-
     val bodyJs = request.body.asJson.getOrElse(Json.parse(""))
     val presenter = (bodyJs \ "presenter").asOpt[String].fold("")(identity)
     val topic = (bodyJs \ "topic").asOpt[String].fold("")(identity)
@@ -43,7 +47,6 @@ class KnolxSessionController @Inject()(knolxSessionService: KnolxSessionService,
   }
 
   private def getSessionData(sessionId: Int, rating: Int): (Option[Int], Option[Int]) = {
-
     val knolxSessionId = sessionId match {
       case x => Some(x)
       case _ => None
@@ -55,5 +58,15 @@ class KnolxSessionController @Inject()(knolxSessionService: KnolxSessionService,
     (knolxSessionId, knolxRating)
   }
 
+  def getAllKnolxSession: Action[AnyContent] = UserAction.async { implicit request =>
+    (knolxSessionService.getAllKnolxSession() map { sessionList =>
+      Ok(jsonResponse.successResponse(Json.obj("sessionList" -> Json.toJson(sessionList))))
+    }) recover {
+      case NonFatal(ex) => {
+        getLogger(this.getClass).error(s"\n\n ${ex.getMessage}")
+        InternalServerError("Internal Server Error")
+      }
+    }
+  }
 
 }
--- backend/web/app/service/KnolxSessionService.scala
+++ backend/web/app/service/KnolxSessionService.scala
@@ -2,7 +2,7 @@ package service
 
 import com.google.inject.Inject
 import com.knoldus.dao.services.user.KnolxSessionDBService
-import com.knoldus.exceptions.PSqlException.InsertionError
+import com.knoldus.exceptions.PSqlException.{DatabaseException, InsertionError}
 import com.knoldus.models.KnolxSession
 
 import scala.concurrent.Future
@@ -16,4 +16,12 @@ class KnolxSessionService @Inject()(val knolxSessionDBService: KnolxSessionDBSer
       case Failure(_) => throw InsertionError("unable to create new knolx session")
     }
   }
+
+  def getAllKnolxSession(): Future[List[KnolxSession]] = {
+  Try (knolxSessionDBService.getAllKnolxSessions()) match {
+    case Success(sessionList) => sessionList
+    case Failure(_) => throw DatabaseException("Unable to fetch data for Knolx session")
+  }
+  }
+
 }
